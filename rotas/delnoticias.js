0f1efac6ef8d40c1d1ea2a3fcadb02bc1efca191
estevao-bohrer <estevaobohrer72@gmail.com>
2025-12-12 08:09:49 -0400

    Add files via upload

---
+++ rotas/delnoticias.js
@@ -0,0 +1,17 @@
+const { ObjectId } = require("mongodb")
+module.exports = (app) => {
+    app.delete('/delnoticias', async (req, res) => {
+        try {
+            const id = ObjectId.createFromHexString(req.body._id)
+            const resultado = await app.DBClient.db('portalnoticias')
+                .collection('noticias')
+                .deleteOne({_id: id})
+            res.status(200).json({ status: 1, msg: "Notícia apagada com sucesso" })
+        } catch (error) {
+            res.status(400).json({ status: 0, msg: "Erro ao apagar notícia" })
+        }
+    })
+
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,34 +1,29 @@
+// Use "type: commonjs" in package.json to use CommonJS modules
 const express = require('express');
-const bodyParser = require("body-parser");
 const cors = require('cors');
 const app = express();
-const port = 3000
+const port = 3000;
 
-app.set("view engine", "ejs")
-app.set("views", "./views")
+app.set('view engine', 'ejs')
+app.set('views', "./views")
 
-app.use(bodyParser.json());
-/* estas linhas são para leitura de de body.json */
 app.use(express.json());
 app.use(express.urlencoded());
-/* aqui é o acesso exerno do sistema para a api */
 
 app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
-//rotas
-require('../db/dbconnect')(app);
-require('../route/home')(app);
-require('../route/getnoticias')(app);
-require('../route/getidnoticias')(app);
-require('../route/gettiponoticias')(app);
-require('../route/delnoticias')(app);
-require('../route/postnoticias')(app);
-require('../route/putnoticias')(app);
-
+require("../db/db")(app);
+require("../rotas/home")(app);
+require("../rotas/getnoticias")(app);
+require("../rotas/getidnoticias")(app);
+require("../rotas/gettiponoticias")(app);
+require("../rotas/putnoticias")(app);
+require("../rotas/postnoticias")(app);
+require("../rotas/delnoticias")(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
