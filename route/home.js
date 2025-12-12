af39f0de7633c3c43def0263c9631e0b3ecc4bbc
estevao-bohrer <estevaobohrer72@gmail.com>
2025-12-12 07:56:51 -0400

    Update home.js

--- route/home.js
+++ route/home.js
@@ -1,6 +1,7 @@
 module.exports = (app)=>{
     app.get("/",(req,res)=>{
-        res.json({ status: "API Online", mensagem: "Bem-vindo à API de Notícias" });
+      res.render('home.ejs');
     })
 
 }
+
